fb1d2a674180113680b268da7c5c1f22a6e887f4
Tabitht <109758130+Tabitht@users.noreply.github.com>
2024-06-12 20:34:37 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,18 +1,19 @@
 const { MongoClient } = require('mongodb')
 
-let host = process.env.DB_HOST;
+// let host = process.env.DB_HOST;
 
-let port = process.env.DB_PORT;
+// let port = process.env.DB_PORT;
 
-let protocol = process.env.DB_CONNECTION;
+// let protocol = process.env.DB_CONNECTION;
 
 let databaseName = process.env.DB_DATABASE;
 
-if (! host || ! port || ! protocol || ! databaseName) {
-    throw new Error('One or more required environment variables (DB_HOST, DB_PORT, DB_CONNECTION, DB_DATABASE) are missing or empty.');
-}
+// if (! host || ! port || ! protocol || ! databaseName) {
+   // throw new Error('One or more required environment variables (DB_HOST, DB_PORT, DB_CONNECTION, DB_DATABASE) are missing or empty.');
+// }
 
-let databaseString = `${protocol}://${host}:${port}` || process.env.MONGODB_URI
+let databaseString = process.env.MONGODB_URI
+    // `${protocol}://${host}:${port}` || process.env.MONGODB_URI
 
 const client = new MongoClient(databaseString);
 
