a1defc7156f3b30c2686c7a1964ccf1eb6c08594
Tabitht <109758130+Tabitht@users.noreply.github.com>
2024-11-19 16:04:19 +0100

    Update EventControllers.js

--- app/controllers/EventControllers.js
+++ app/controllers/EventControllers.js
@@ -24,12 +24,14 @@ async function create(request, response) {
         await client.connect();
         const db = client.db('Event_Management-App');
         const bucket = new GridFSBucket(db, { bucketName: 'uploads' });
-       // if (!request.file) {
-        //    return response.status(400).json({ 'data': { 'error': 'File is required' } });
-      //  }
+        //if (!request.file) {
+            //readableStream.end(null);;
+        }
 
         const readableStream = new stream.PassThrough();
-        readableStream.end(request.file ? request.file.buffer : null);
+        readableStream.end(request.file.buffer);
+        if (!request.file) {
+            readableStream.end(null);
 
         const uploadStream = bucket.openUploadStream(request.file.originalname, {
             contentType: request.file.mimetype
